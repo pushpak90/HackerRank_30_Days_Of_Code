f399aa09961e0187489cf9d1b34026ab15cbd678
pushpak90 <pushpakafasate90@gmail.com>
2022-08-15 17:45:09 +0530

    Day 16

---
+++ Day16_ExceptionsStringtoInteger.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    try {
+        cout << stoi(s) << endl;
+    } catch(...) {
+        cout << "Bad String" << endl;
+    }
+    
+    return 0;
+
+    return 0;
+}
